77065e27f3fb9fafba4d042bc2683c476d96ce2d
Daffa Putera Kouseina <kouseinadaffa@gmail.com>
2020-02-22 19:05:23 +0700

    implementasi push message

--- public/index.php
+++ public/index.php
@@ -83,4 +83,20 @@ $app->post('/webhook', function (Request $request, Response $response) use ($cha
         }
     }
 });
+
+$app->get('/pushmessage', function ($req, $response) use ($bot) {
+    // send push message to user
+    $userId = 'U1afb282d2e367cc2396fbf1d8716a5c7';
+    $textMessageBuilder = new TextMessageBuilder('Halo, ini pesan push');
+    $result = $bot->pushMessage($userId, $textMessageBuilder);
+
+    $stickerMessageBuilder = new StickerMessageBuilder(1, 106);
+    $bot->pushMessage($userId, $stickerMessageBuilder);
+
+    $response->getBody()->write((string) $result->getJSONDecodedBody());
+    return $response
+        ->withHeader('Content-Type', 'application/json')
+        ->withStatus($result->getHTTPStatus());
+});
+
 $app->run();
