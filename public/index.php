e8d73b86d94e5566595b5d415e9d26c16ae6dca4
Daffa Putera Kouseina <kouseinadaffa@gmail.com>
2020-02-25 07:30:34 +0700

    implementasi reply message remake

--- public/index.php
+++ public/index.php
@@ -119,10 +119,8 @@ $app->post('/webhook', function (Request $request, Response $response) use ($cha
 
                         if ((strtolower($event['message']['text']) == 'kamu siapa') or (strtolower($event['message']['text']) == 'kenalin diri dong') or (strtolower($event['message']['text']) == 'kamu siapa?')) {
 
-                            $result = $bot->replyText(
-                                $event['replyToken'],
-                                " yang Anda kirim bisa diakses dari link"
-                            );
+                            $greetings = new TextMessageBuilder("Halo, ");
+                            $result = $bot->replyMessage($event['replyToken'], $greetings);
 
                             $response->getBody()->write((string) $result->getJSONDecodedBody());
                             return $response
