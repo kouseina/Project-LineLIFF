118107b332481096ab68a44fae68a7af0097484a
Daffa Putera Kouseina <kouseinadaffa@gmail.com>
2020-02-25 07:17:47 +0700

    implementasi reply message remake

--- public/index.php
+++ public/index.php
@@ -155,11 +155,10 @@ $app->post('/webhook', function (Request $request, Response $response) use ($cha
                             $result = $bot->replyText($event['replyToken'], $event['source']['userId']);
                         } elseif ((strtolower($event['message']['text']) == 'kamu siapa') or (strtolower($event['message']['text']) == 'kenalin diri dong') or (strtolower($event['message']['text']) == 'kamu siapa?')) {
 
-                            $contentURL = "https://implementlinephp.herokuapp.com/public/content/" . $event['message']['id'];
                             $contentType = ucfirst($event['message']['type']);
                             $result = $bot->replyText(
                                 $event['replyToken'],
-                                $contentType . " yang Anda kirim bisa diakses dari link:\n " . $contentURL
+                                $contentType . " yang Anda kirim bisa diakses dari link"
                             );
 
                             $response->getBody()->write((string) $result->getJSONDecodedBody());
